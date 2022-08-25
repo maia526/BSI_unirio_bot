f76d4e04bde54ad0196e40ff3aceac683d537272
Mariana Maia <96018595+maia526@users.noreply.github.com>
2022-08-25 13:05:08 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,36 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+
 import requests as re
 from bs4 import BeautifulSoup
+
+#especifica a qual bot o código se refere (dentro dos parênteses está o token do bot)
+updater = Updater("5589909898:AAGo3Bwcy2hZ6P6DrM0FSbQDxlg24tGptok",
+                  use_context=True)
+
+#mensagem de boas vindas que aparece quando o usuário iniciar a interação com o bot
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("O BSI_unirio_bot te dá boas vindas! Digite /ajuda para ver os comandos disponíveis.")
+
+#mensagem com os comandos que o bot aceita (ex.: calendario, horarios...)
+#A ALTERAR
+def ajuda(update: Update, context: CallbackContext):
+	update.message.reply_text("Your Message")
+
+
+#mensagem que aparece se usuário digitar comando inválido
+def desconhecido(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Desculpe, '%s' não é um comando válido." % update.message.text)
+#mensagem que aparece se o usuário digitar algo que não é um comando
+def texto_desconhecido(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Desculpe, eu não entendi, você falou: '%s'" % update.message.text)
+
 
 def retorna_lista_datas():
     url = 'http://www.unirio.br/prograd/calendario-academico'
@@ -17,3 +48,16 @@ datas = retorna_lista_datas()
 
 for data in datas:
     print(data)
+
+#se usuário digitar o que está no primeiro parâmetro, a função do segundo parâmetro é rodada
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('ajuda', ajuda))
+
+updater.dispatcher.add_handler(CommandHandler('calendario', retorna_lista_datas))
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, desconhecido))
+updater.dispatcher.add_handler(MessageHandler(Filters.command, desconhecido)) #filtra comandos desconhecidos
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, texto_desconhecido)) #filtra mensagens desconhecidas
+
+updater.start_polling()
