dbc173f2f94bb0a2f27411802ca6cdba5b499e19
matEvangelista <mateus.e.alcantara@outlook.com>
2022-08-30 23:47:47 -0300

    removendo código desnecessário'

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
 from ast import parse
-import encodings
 from encodings.utf_8 import encode
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.filters import Filters
-from datetime import datetime, timedelta
+from datetime import datetime
 import requests as re
 from bs4 import BeautifulSoup
 import csv
